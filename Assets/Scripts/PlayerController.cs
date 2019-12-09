2995b90015d91cfaee8388c6413a09bd8259ffe7
Supratim Ray <ray_supratim@yahoo.com>
2019-12-10 01:44:06 +0530

    Updated colliders.  Define player action. Accept trigger enter. Accept player action.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,20 +17,22 @@ namespace SaladChef
         [SerializeField]
         private string mVerticalAxis = string.Empty;
         [SerializeField]
-        private string mActionKey = string.Empty;
+        private string mAction = string.Empty;
 
         private CharacterController mCharController;
         private bool mAllowInput;
         private bool mInitialized;
 
+        private Collider mCurrentCollider;
+
         void Start()
         {
             mCharController = GetComponent<CharacterController>();
             mInitialized = true;
-            PlayerPause(false);
+            StopPlayerMovement(false);
         }
 
-        public void PlayerPause(bool value)
+        public void StopPlayerMovement(bool value)
         {
             mAllowInput = !value;
         }
@@ -45,13 +47,33 @@ namespace SaladChef
             mSpeed = speed;
         }
 
-        void HandleInput()
+        private void HandleInput()
         {
             if (mAllowInput && mInitialized)
             {
                 Vector3 displacement = new Vector3(Input.GetAxis(mHorizontalAxis), 0, Input.GetAxis(mVerticalAxis)) * mSpeed * Time.deltaTime;
                 mCharController.Move(displacement);
+
+                if (Input.GetButtonDown(mAction))
+                    OnPlayerInteraction();
             }
+        }
+
+        private void OnPlayerInteraction()
+        {
+            Debug.Log("on player action:: " + name);
+        }
+
+        private void OnTriggerEnter(Collider collider)
+        {
+            mCurrentCollider = collider;
+            Debug.Log("collider enter:: " + mCurrentCollider.name);
+        }
+
+        private void OnTriggerExit(Collider collider)
+        {
+            Debug.Log("collider exit:: " + collider.name);
+            mCurrentCollider = null;            
         }
     }
 }
