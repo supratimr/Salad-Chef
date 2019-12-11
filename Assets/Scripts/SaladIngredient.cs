35ca268aa3b2beca9cc0e07d331871483b9a487b
Supratim Ray <ray_supratim@yahoo.com>
2019-12-11 16:29:15 +0530

    Config Data for ingedients and salads.

---
+++ Assets/Scripts/SaladIngredient.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+    public enum IngredientTypes
+    {
+        Cucumber,
+        Tomato,
+        Broccoli,
+        Carrot,
+        Avocado,
+        Apple
+    }
+
+    [System.Serializable]
+    public class IngredientData
+    {
+        public IngredientTypes Type;
+        public string DisplayName;
+        public GameObject MainObject;
+        public GameObject ChoppedObject;
+        public Sprite Icon;
+        public float ChopDuration;
+    }
+
+    [System.Serializable]
+    public class Salad
+    {
+        public string SaladName;
+        public Sprite Icon;
+        public List<IngredientTypes> IngredientList = new List<IngredientTypes>();
+    }
+
+    [CreateAssetMenu(fileName = "SaladIngredientConfig", menuName = "Config/SaladIngredientConfig", order = 1)]
+    public class SaladIngredientConfig: ScriptableObject
+    {
+        public IngredientData[] IngredientData;
+        public Salad[] SaladData;
+    }
+
